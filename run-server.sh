d0432ebce2238ce7cd3c6cc0e49403e3d0b854cb
snafuz <snafuz.and@gmail.com>
2018-02-08 10:46:45 +0100

    manage server as daemon

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -d --tty --rm -p 5000:5000 --volume "$PWD":/data cloudshowrome "$@"
+#
+#run the server as daemon use docker ps to see the status
+docker run -d --tty --rm -p 5000:5000 --volume "$PWD":/data cloudshowrome "$@" > .apiserver.pid
